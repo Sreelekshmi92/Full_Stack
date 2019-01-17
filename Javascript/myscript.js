3e8514031645c521a9136b0cfad83d03454786a3
unknown <Administrator@Asiet-Lab.ASIETDomain.com>
2019-01-17 10:59:46 +0530

    bootstrap added

---
+++ Javascript/myscript.js
@@ -0,0 +1,21 @@
+var myName="Anu";
+console.log(myName);
+var a=21;
+var b=30;
+var c=a+b;
+console.log(c);
+var x=2;
+var y=3;
+var z=x**y;
+console.log(z);
+var q=10;  
+var r=10;
+console.log(++q);
+console.log(r--);
+const p=3;
+console.log(p);
+{
+    let n=5; //local access
+    n=n+5;
+console.log(n);
+}
