bdeb6a3c435049f325712154f8492887cc5ce329
unknown <Administrator@Asiet-Lab.ASIETDomain.com>
2019-01-31 11:02:51 +0530

    Day3

--- MEAN/Day3/app.js
+++ MEAN/Day3/app.js
@@ -1,6 +1,8 @@
 var express=require('express');
 const app=express();
 var mongodb=require('mongodb');
+var bodyparser=require('body-parser')
+app.use(bodyparser.urlencoded({extended:true}))
 
 var mongoclient=mongodb.MongoClient;
 
@@ -13,14 +15,15 @@ app.get("/",function(req,res){
     res.render("Home");
 })
 
-app.get("/insert",function(req,res){
+app.post("/insert",function(req,res){
 
     mongoclient.connect(url,function(err,database){
 
         var dtb=database.db('Empdb'); //connecting to db
         var collections=dtb.collection('employee'); //connecting to table
         var data={"Eid" : "2001", "Ename" : "Sanju", "Dept" : "hr", "Salary" : 12000, "location" : "tcr", "Dob" : "29-01-1992"}
-        collections.insert(data,function(err,result){
+        var data1={"Ename":req.body.empname,"Salary":req.body.salary}
+        collections.insert(data1,function(err,result){
             if(err){res.send("Error");}
             else{res.send("Data inserted successfully");}
         });
